e3391718c7f0c72ac2d527a3b88b6dde74085b35
Justin Fuentes <jfuen114@gmail.com>
2024-04-01 16:31:31 -0400

    This is my first commit to my freeRTOS demo :D

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+void task() {
+    for(size_t i = 0; i < 5; i++) {
+        printf("task %d: %lu\n", i, xTaskGetTickCount() * portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main() {
+    // char *ourTaskName = pcTaskGetName;
+    // ESP_LOGI(ourTaskName, "Hello, World!");
+
+
+    // int count = 0;
+    // while(true) {
+    //     ESP_LOGI("QEMU", "running on QEMU count %d", count++);
+    //     vTaskDelay(pdMS_TO_TICKS(1000));
+    // }
+    printf("\nTimer output in milliseconds program initiation: %lu\n\n", xTaskGetTickCount()* portTICK_PERIOD_MS);
+
+    xTaskCreate(task, "mytask", 2048, NULL, 1, NULL);
+
+    printf("\nProgram completion: %lu\n", xTaskGetTickCount()* portTICK_PERIOD_MS);
+}
