963e35c112e4d02af63466f78c0970b340c63060
Justin Fuentes <jfuen114@gmail.com>
2024-04-07 20:46:18 -0400

    Added the framework to begin the CAN communication using ESP-IDF TWAI driver :)

--- src/main.c
+++ src/main.c
@@ -20,9 +20,21 @@ void task() {
     }
     vTaskDelete(NULL);
 }
-    //testSS
-void app_main() {
+
+//In this task, we will attempt listening to a CAN signal and perhaps demodding it.
+void can_listen() {
     
+
+}
+
+void app_main() {
+    //Here we will use the document specific TWAI driver initialization to begin a driver for CAN communciation.
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_10, GPIO_NUM_9, TWAI_MODE_LISTEN_ONLY);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    printf("Attempting TWAI driver, install code :%x\n", twai_driver_install(&g_config, &t_config, &f_config));
+    printf("Attempting to start TWAI driver, start code :%x\n",   twai_start()); 
     
     gpio_set_direction(PIN_CAN_HIGH, GPIO_MODE_INPUT);
     // int count = 0;
